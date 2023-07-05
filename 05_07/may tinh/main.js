001afc255bc80edd08262d4d16cddf9357667c54
vietlongbkdp <vietlongbkdp@gmail.com>
2023-07-06 01:01:23 +0700

    update code

---
+++ 05_07/may tinh/main.js
@@ -0,0 +1,72 @@
+var string_text = document.getElementById("screen").innerText;
+function get_value_1(){
+    var a = document.getElementById("btn_1").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_2(){
+    var a = document.getElementById("btn_2").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_3(){
+    var a = document.getElementById("btn_3").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_4(){
+    var a = document.getElementById("btn_4").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_5(){
+    var a = document.getElementById("btn_5").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_6(){
+    var a = document.getElementById("btn_6").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_7(){
+    var a = document.getElementById("btn_7").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_8(){
+    var a = document.getElementById("btn_8").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_9(){
+    var a = document.getElementById("btn_9").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_0(){
+    var a = document.getElementById("btn_0").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_00(){
+    var a = document.getElementById("btn_00").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_dot(){
+    var a = document.getElementById("btn_dot").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_dv(){
+    var a = document.getElementById("btn_dv").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_pl(){
+    var a = document.getElementById("btn_pl").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_X(){
+    var a = document.getElementById("btn_X").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_sub(){
+    var a = document.getElementById("btn_sub").innerText
+    document.getElementById("screen").innerText = string_text + a;
+}
+function get_value_clear(){
+    var a = document.getElementById("clear").innerText
+    document.getElementById("screen").innerText = a;
+}
+
+
+
