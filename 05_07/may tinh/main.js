e58086d8ae9711d3a25170a5a9efc90f8af6eefa
vietlongbkdp <vietlongbkdp@gmail.com>
2023-07-11 08:54:33 +0700

    update file

--- 05_07/may tinh/main.js
+++ 05_07/may tinh/main.js
@@ -9,19 +9,15 @@ function get_value(get__id){
     var string_text = document.getElementById("screen");
     // console.log(string_text);
     var a = document.getElementById(get__id).innerText;
-    if(a == "+")
-    {
-        c = c + (+string_text.innerHTML);
-       console.log(+string_text.innerHTML);
-       console.log("tổng là: ",c);
-       document.getElementById("screen").innerText = "";
-    }
-    else{
+
     string_text.innerHTML += a;
     c = c +  (+string_text.innerHTML);
-    }
 
 }
 function Result(){
-    document.getElementById("screen").innerText = c;
+    let str = document.getElementById("screen").value;
+    console.log(str);
+    var Res_1 = eval(str);
+    console.log(Res_1);
+    document.getElementById("screen").innerText = Res_1;
  }
