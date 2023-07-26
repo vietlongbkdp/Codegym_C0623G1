7a5d65b3426c58e640078400d6169071f3a33c90
vietlongbkdp <vietlongbkdp@gmail.com>
2023-07-27 00:04:12 +0700

    update code

---
+++ 27_07/main.js
@@ -0,0 +1,105 @@
+
+
+// var arr = ['4','2','5','1','6','5', '4']
+
+// let newArr = arr.push('7'); //['4', '2', '5','1', '6', '4','7']
+
+// let newArr = arr.join(); //4,2,5,1,6,4
+
+// let newArr = arr.join(' ');//4 2 5 1 6 4 **
+
+// let newArr = arr.toString();// 4,2,5,1,6,4
+
+// let newArr = arr.toString('   ');// 4,2,5,1,6,4**
+
+// let newArr = arr.toString();
+
+// let newArr = arr.slice(1,3) //[ '2', '5' ]
+
+// let newArr = arr.splice(1,3);// [ '2', '5', '1' ] -----arr = [ '4', '6', '4' ]
+
+// let newArr = arr.unshift('1','2','3','4')//    arr = ['1', '2', '3', '4','4', '2', '5', '1','6', '4']  ---- return 10
+
+
+// let newArr = arr.sort(); // [ '1', '2', '4', '4', '5', '6' ]
+
+// let newArr = arr.shift();// xoá phần tử đầu tiên của mảng
+
+// let newArr = arr.pop(); //[ '4', '2', '5', '1', '6' ]
+
+// let newArr = arr.reverse(); //[ '4', '6', '1', '5', '2', '4' ]
+
+// let newArr = arr.lastIndexOf('5');// return 2 (tìm vị trí phần tử '5' cuối cùng trong mảng)
+
+
+// var projec1 = ["PHP", "CSS"];
+// var projec2 = ["HTML", "Python", "JS"];
+// var children = projec1.concat(projec2); 
+// console.log(children); // PHP,CSS,HTML,Python,JS
+
+// let newArr = arr.IndexOf('5' ,0);
+
+// console.log(arr);
+// console.log(newArr);
+
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.push("Kiwi"); //Banana,Orange,Apple,Mango,Kiwi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var str='Chào Quang đẹP traI, QuAng đI qUang đó, nói Gì đi Chứ Quang';
+
+// let arr = str.split('');
+// var count = 0;
+// // for (let i = 0; i < arr.length; i++) {
+// //     if(arr[i] == 'Quang'){
+// //         count++;
+// //     }
+// // }
+// // console.log(`Có ${count} chữ Quang trong câu`)
+
+
+// // console.log(arr);
+
+
+// var strModel = 'qwertyuiopasdfghjklzxcVBNM';
+// var strModelUp = strModel.toLocaleUpperCase();
+// var arrComp = strModelUp.split('');//i
+// var arrStr = str.split('');//j
+// for (let i = 0; i < arrStr.length; i++) {
+//     for( let j= 0; j<arrComp.length; j++){
+//         if(arrComp[j] === arrStr[i]){
+//             count++;
+//         }
+//     }
+    
+// }
+// console.log(`Có ${count} chữ cái viết hoa trong câu`);
+
+
+// // console.log(arrComp);
+
+
+
+
+
+
+
+
+
+
+
+
